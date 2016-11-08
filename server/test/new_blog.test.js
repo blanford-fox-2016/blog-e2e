cf853e851ef56fb01f36a04acb4728eca985c0c0
alexanderHT <alexanderhendrawan@gmail.com>
2016-11-09 01:49:51 +0700

    update 3

--- server/test/new_blog.test.js
+++ server/test/new_blog.test.js
@@ -3,7 +3,7 @@ var chai = require('chai');
 var expect = require('chai').expect; // jshint ignore:line
 var chaiHttp = require('chai-http');
 
-let cheerio = require('cheerio')
+let cheerio = require('cheerio') // need req cheerio for convert
 
 // $('h2.title').text('Hello there!')
 // $('h2').addClass('welcome')
@@ -20,32 +20,87 @@ var nightmare = Nightmare({
 
 chai.use(chaiHttp);
 
-describe('test blogs', function() {
-
+describe('Test CRUD Blogs', function() {
+  // set time out for test
   this.timeout(500000)
+  // convert js to tag html
   const $ = cheerio
-  it('it should create new blog', function(done) {
-    nightmare
+
+  // it('it should create new blog', function(done) {
+  //
+  //     nightmare // req nightmare
+  //     .goto('http://127.0.0.1:8080/blog.html')
+  //     .type('input#blogID', '2')
+  //     .wait(1000)
+  //     .type('input#title', 'There always be hope')
+  //     .wait(1000)
+  //     .type('textarea#content', 'what you need to do is keep fight until the end')
+  //     .wait(1000)
+  //     .click('#addNewBlog')
+  //     .wait(1000)
+  //     .evaluate(function () {
+  //       let val = $('#listOfBlog tr:last .title', document).text()// using cherrio
+  //       return val
+  //     })
+  //     .then(function(data) {
+  //       // console.log(">>>>>", data);
+  //       expect(data).to.equal('There always be hope');
+  //       done();
+  //     })
+  //     .catch(function(err) {
+  //       console.log(err);
+  //     })
+  // });
+
+  it('it should delete the last blog', function(done) {
+
+      nightmare // req nightmare
+
       .goto('http://127.0.0.1:8080/blog.html')
-      .type('input#blogID', '2')
       .wait(1000)
-      .type('input#title', 'There always be hope')
-      .wait(1000)
-      .type('textarea#content', 'what you need to do is keep fight until the end')
-      .wait(1000)
-      .click('input#addNewBlog')
-      .wait(1000)
+      // select the button deletes
+      // $('#listOfBlog tr:last button')[1]
+      .click('#listOfBlog tr:last .btn-danger')
       .evaluate(function () {
-        let val = $('#listOfBlog tr:last .title', document).text()// using cherrio
-        return val
+        // let val = $('#listOfBlog tr:last .title', document).text() // using cherrio
+        // return val
       })
-      .then(function(data) {
-        console.log(">>>>>", data);
-        expect(data).to.equal('There always be hope');
+      .then(function() {
+        // console.log(">>>>>", data);
+        // expect(data).to.equal('There always be hope');
         done();
       })
       .catch(function(err) {
         console.log(err);
       })
   });
+
+  // it('it should edit the last blog', function(done) {
+  //
+  //     nightmare // req nightmare
+  //
+  //     .goto('http://127.0.0.1:8080/blog.html')
+  //     .wait(1000)
+  //     .click('#edit1')
+  //     .type('input#title', 'There always be hope 2')
+  //     .wait(1000)
+  //     .type('textarea#content', 'what you need to do is keep fight until the end 2')
+  //     .wait(1000)
+  //     .click('#updateButton')
+  //     .evaluate(function () {
+  //       // let val = $('#listOfBlog tr:last .title', document).text() // using cherrio
+  //       // return val
+  //     })
+  //     .then(function() {
+  //       // console.log(">>>>>", data);
+  //       // expect(data).to.equal('There always be hope');
+  //       done();
+  //     })
+  //     .catch(function(err) {
+  //       console.log(err);
+  //     })
+  // });
+
+
+
 });
